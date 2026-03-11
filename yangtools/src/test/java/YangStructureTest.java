eb40ecb8e56f0779e5d52ae8eb9ec8aa002c58f6
Vivek <vivekananda.boudia@gmail.com>
2026-03-11 17:43:50 +0100

    structure test
    not working now

--- yangtools/src/test/java/YangStructureTest.java
+++ yangtools/src/test/java/YangStructureTest.java
@@ -1,9 +1,127 @@
+import com.google.gson.stream.JsonReader;
 import org.junit.jupiter.api.Test;
+import org.opendaylight.yangtools.yang.data.codec.gson.JSONCodecFactorySupplier;
+import org.opendaylight.yangtools.yang.data.codec.gson.JsonParserStream;
+import org.opendaylight.yangtools.yang.data.impl.schema.ImmutableNormalizedNodeStreamWriter;
+import org.opendaylight.yangtools.yang.data.impl.schema.NormalizationResultHolder;
+import org.opendaylight.yangtools.yang.model.api.EffectiveModelContext;
+
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class YangStructureTest {
 
     @Test
-    void testYangStructure(){
-        //TODO
+    void testYangStructure() throws Exception {
+        List<String> schemaFile = List.of("../yang/ietf-yang-structure-ext.yang", "../yang/structure.yang");
+        EffectiveModelContext schema = YangToolsUtils.loadSchema(schemaFile);
+        assertNotNull(schema);
+    }
+
+    @Test
+    void testYangStructureMinimal() throws Exception {
+        List<String> schemaFile = List.of("../yang/struct-test.yang");
+        EffectiveModelContext schema = YangToolsUtils.loadSchema(schemaFile);
+        assertNotNull(schema);
+    }
+
+    @Test
+    void testYangStructureScottHuangValidMinimal() throws Exception {
+        List<String> schemaFile = List.of("../yang/scotthuang-structure.yang", "../yang/ietf-yang-types.yang", "../yang/ietf-yang-structure-ext.yang");
+        EffectiveModelContext schema = YangToolsUtils.loadSchema(schemaFile);
+        assertNotNull(schema);
+
+        NormalizationResultHolder resultHolder = new NormalizationResultHolder();
+        var writer = ImmutableNormalizedNodeStreamWriter.from(resultHolder);
+
+        var parser = JsonParserStream.create(
+                writer,
+                JSONCodecFactorySupplier.RFC7951.getShared(schema)
+        );
+
+        assertDoesNotThrow(() -> {
+            try (JsonReader reader = new JsonReader(
+                    new InputStreamReader(
+                            Files.newInputStream(Paths.get("../data/scotthuang_valid_structure_minimal.json"))
+                    ))) {
+                parser.parse(reader);
+            }
+        });
+    }
+
+    @Test
+    void testYangStructureScottHuangValid() throws Exception {
+        List<String> schemaFile = List.of("../yang/scotthuang-structure.yang", "../yang/ietf-yang-types.yang", "../yang/ietf-yang-structure-ext.yang");
+        EffectiveModelContext schema = YangToolsUtils.loadSchema(schemaFile);
+        assertNotNull(schema);
+
+        NormalizationResultHolder resultHolder = new NormalizationResultHolder();
+        var writer = ImmutableNormalizedNodeStreamWriter.from(resultHolder);
+
+        var parser = JsonParserStream.create(
+                writer,
+                JSONCodecFactorySupplier.RFC7951.getShared(schema)
+        );
+
+        assertDoesNotThrow(() -> {
+            try (JsonReader reader = new JsonReader(
+                    new InputStreamReader(
+                            Files.newInputStream(Paths.get("../data/scotthuang_valid_structure_message.json"))
+                    ))) {
+                parser.parse(reader);
+            }
+        });
+    }
+
+    @Test
+    void testYangStructureScottHuangInvalid() throws Exception {
+        List<String> schemaFile = List.of("../yang/scotthuang-structure.yang", "../yang/ietf-yang-types.yang", "../yang/ietf-yang-structure-ext.yang");
+        EffectiveModelContext schema = YangToolsUtils.loadSchema(schemaFile);
+        assertNotNull(schema);
+
+        NormalizationResultHolder resultHolder = new NormalizationResultHolder();
+        var writer = ImmutableNormalizedNodeStreamWriter.from(resultHolder);
+
+        var parser = JsonParserStream.create(
+                writer,
+                JSONCodecFactorySupplier.RFC7951.getShared(schema)
+        );
+
+        assertThrows(Exception.class, () -> {
+            try (JsonReader reader = new JsonReader(
+                    new InputStreamReader(
+                            Files.newInputStream(Paths.get("../data/scotthuang_invalid_structure_type.json"))
+                    ))) {
+                parser.parse(reader);
+            }
+        });
+    }
+
+    @Test
+    void testYangStructureScottHuangInvalidMissingMandatory() throws Exception {
+        List<String> schemaFile = List.of("../yang/scotthuang-structure.yang", "../yang/ietf-yang-types.yang", "../yang/ietf-yang-structure-ext.yang");
+        EffectiveModelContext schema = YangToolsUtils.loadSchema(schemaFile);
+        assertNotNull(schema);
+
+        NormalizationResultHolder resultHolder = new NormalizationResultHolder();
+        var writer = ImmutableNormalizedNodeStreamWriter.from(resultHolder);
+
+        var parser = JsonParserStream.create(
+                writer,
+                JSONCodecFactorySupplier.RFC7951.getShared(schema)
+        );
+
+        assertThrows(Exception.class, () -> {
+            try (JsonReader reader = new JsonReader(
+                    new InputStreamReader(
+                            Files.newInputStream(Paths.get("../data/scotthuang_invalid_structure_missing_mandatory.json"))
+                    ))) {
+                parser.parse(reader);
+            }
+        });
     }
 }
--- yangtools/src/test/java/YangToolsUtils.java
+++ yangtools/src/test/java/YangToolsUtils.java
@@ -1,5 +1,8 @@
+import org.opendaylight.yangtools.rfc8791.model.api.StructureEffectiveStatement;
 import org.opendaylight.yangtools.yang.model.api.EffectiveModelContext;
+import org.opendaylight.yangtools.yang.model.api.EffectiveStatementInference;
 import org.opendaylight.yangtools.yang.model.spi.source.FileYangTextSource;
+import org.opendaylight.yangtools.yang.model.util.SchemaInferenceStack;
 import org.opendaylight.yangtools.yang.parser.api.YangParserFactory;
 
 import java.nio.file.Paths;
@@ -20,6 +23,31 @@ public final class YangToolsUtils {
         }
 
         return parser.buildEffectiveModel();
+    }
+
+    public static StructureEffectiveStatement findStructure(
+            EffectiveModelContext schemaContext,
+            String moduleName,
+            String structureName
+    ) {
+        var module = schemaContext.getModuleStatements().values().stream()
+                .filter(m -> m.argument().getLocalName().equals(moduleName))
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException("Module not found: " + moduleName));
+
+        return module.findFirstEffectiveSubstatement(StructureEffectiveStatement.class)
+                .filter(stmt -> stmt.argument().getLocalName().equals(structureName))
+                .orElseThrow(() -> new IllegalStateException(
+                        "Structure not found: " + moduleName + ":" + structureName));
+    }
+
+    public static EffectiveStatementInference toInference(
+            EffectiveModelContext schemaContext,
+            StructureEffectiveStatement structure
+    ) {
+        var stack = SchemaInferenceStack.of(schemaContext);
+        stack.enterSchemaTree(structure.argument());
+        return stack.toInference();
     }
 
 }
