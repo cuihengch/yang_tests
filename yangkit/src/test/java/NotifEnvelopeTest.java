fc7990dacd807238a1884bbc3f4f88993788f115
BVivek974 <vivekananda.boudia@gmail.com>
2026-04-21 19:31:45 +0200

    add telemetry and notif envelope test

--- yangkit/src/test/java/NotifEnvelopeTest.java
+++ yangkit/src/test/java/NotifEnvelopeTest.java
@@ -1,16 +1,38 @@
+import com.fasterxml.jackson.databind.JsonNode;
 import org.dom4j.DocumentException;
 import org.junit.jupiter.api.Test;
+import org.yangcentral.yangkit.common.api.validate.ValidatorResult;
+import org.yangcentral.yangkit.model.api.schema.YangSchemaContext;
 import org.yangcentral.yangkit.parser.YangParserException;
 
 import java.io.IOException;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class NotifEnvelopeTest {
 
     @Test
-    void testLoadValidSchema() throws DocumentException, IOException, YangParserException {
+    void testValidNotifEnvelope() throws DocumentException, IOException, YangParserException {
+        YangSchemaContext schemaContext = YangkitUtils.loadSchema("../yang/notif");
+        JsonNode validData = YangkitUtils.loadJson("../data/valid_notification.json");
+        ValidatorResult schemaValidation = YangkitUtils.validateSchema(schemaContext);
+        assertTrue(schemaValidation.isOk());
+        ValidatorResult firstDataValidation = YangkitUtils.parsingData(schemaContext, validData);
+        assertTrue(firstDataValidation.isOk());
+        ValidatorResult secondDataValidation = YangkitUtils.validateData(schemaContext, validData);
+        assertTrue(secondDataValidation.isOk());
     }
 
     @Test
-    void testLoadInvalidSchema() {
+    void testInvalidNotifEnvelope() throws DocumentException, IOException, YangParserException {
+        YangSchemaContext schemaContext = YangkitUtils.loadSchema("../yang/notif");
+        JsonNode validData = YangkitUtils.loadJson("../data/invalid_notification.json");
+        ValidatorResult schemaValidation = YangkitUtils.validateSchema(schemaContext);
+        assertTrue(schemaValidation.isOk());
+        ValidatorResult firstDataValidation = YangkitUtils.parsingData(schemaContext, validData);
+        assertFalse(firstDataValidation.isOk());
+        ValidatorResult secondDataValidation = YangkitUtils.validateData(schemaContext, validData);
+        assertTrue(secondDataValidation.isOk());
     }
 }
--- yangkit/src/test/java/TelemetryMessageTest.java
+++ yangkit/src/test/java/TelemetryMessageTest.java
@@ -1,16 +1,39 @@
+import com.fasterxml.jackson.databind.JsonNode;
 import org.dom4j.DocumentException;
 import org.junit.jupiter.api.Test;
+import org.yangcentral.yangkit.common.api.validate.ValidatorResult;
+import org.yangcentral.yangkit.model.api.schema.YangSchemaContext;
 import org.yangcentral.yangkit.parser.YangParserException;
 
 import java.io.IOException;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class TelemetryMessageTest {
 
     @Test
-    void testLoadValidSchema() throws DocumentException, IOException, YangParserException {
+    void testValidTelemetryMsg() throws DocumentException, IOException, YangParserException {
+        YangSchemaContext schemaContext = YangkitUtils.loadSchema("../yang/telemetry");
+        JsonNode validData = YangkitUtils.loadJson("../data/valid-telemetry-msg.json");
+        ValidatorResult schemaValidation = YangkitUtils.validateSchema(schemaContext);
+        assertTrue(schemaValidation.isOk());
+        ValidatorResult firstDataValidation = YangkitUtils.parsingData(schemaContext, validData);
+        assertTrue(firstDataValidation.isOk());
+        ValidatorResult secondDataValidation = YangkitUtils.validateData(schemaContext, validData);
+        assertTrue(secondDataValidation.isOk());
     }
 
     @Test
-    void testLoadInvalidSchema() {
+    void testInvalidTelemetryMsg() throws DocumentException, IOException, YangParserException {
+        YangSchemaContext schemaContext = YangkitUtils.loadSchema("../yang/telemetry");
+        JsonNode validData = YangkitUtils.loadJson("../data/invalid-telemetry-msg.json");
+        ValidatorResult schemaValidation = YangkitUtils.validateSchema(schemaContext);
+        assertTrue(schemaValidation.isOk());
+        ValidatorResult firstDataValidation = YangkitUtils.parsingData(schemaContext, validData);
+        assertFalse(firstDataValidation.isOk());
+        ValidatorResult secondDataValidation = YangkitUtils.validateData(schemaContext, validData);
+        assertTrue(secondDataValidation.isOk());
     }
+
 }
